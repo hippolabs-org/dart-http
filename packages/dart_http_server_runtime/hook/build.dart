e526f39ef03d0b0495b42527ceacab38d4869f5c
flowhorn <felixweuth@gmail.com>
2026-08-23 14:14:51 +0200

    refactor: use shared native asset hook

--- packages/dart_http_server_runtime/hook/build.dart
+++ packages/dart_http_server_runtime/hook/build.dart
@@ -1,18 +1,14 @@
-import 'package:code_assets/code_assets.dart';
+import 'package:hippolabs_native_assets/hippolabs_native_assets.dart';
 import 'package:hooks/hooks.dart';
-import 'package:native_toolchain_rust/native_toolchain_rust.dart';
 
 Future<void> main(List<String> args) async {
   await build(args, (input, output) async {
-    if (!input.config.buildCodeAssets) {
-      return;
-    }
-
     final packageName = input.packageName;
 
-    await RustBuilder(
+    await HippolabsRustBuilder(
       assetName: '$packageName.dart',
       cratePath: 'rust',
+      prebuilt: const HippolabsRustPrebuilt.github(),
     ).run(input: input, output: output);
   });
 }
